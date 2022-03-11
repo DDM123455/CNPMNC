062443f7d02407ad8e77c549b80b9a1bb901b06c
DDM123455 <101332133+DDM123455@users.noreply.github.com>
2022-03-11 15:59:42 +0700

    bản 2

---
+++ Thuchanhcnpm/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+
+namespace Thuchanhcnpm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Dương Đăng Mạnh");
+        }
+    }
+}
